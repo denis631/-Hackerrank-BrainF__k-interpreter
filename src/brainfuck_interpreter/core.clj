513f0d6ecb4c4b6ef25dca53583f236d7e3253ab
Denis Grebennicov <denis.grebennicov@check24.de>
2018-05-10 11:11:45 +0200

    AST. Runtime-metadata

--- src/brainfuck_interpreter/core.clj
+++ src/brainfuck_interpreter/core.clj
@@ -1,63 +1,104 @@
 (ns brainfuck-interpreter.core
   (:gen-class))
 
-;; Example programs
-(def hello-world-program "+++++ +++++ [ > +++++ ++ > +++++ +++++ > +++ > + <<<< - ] > ++ . > + . +++++ ++ . . +++ . > ++ . << +++++ +++++ +++++ . > . +++ . ----- - . ----- --- . > + .")
+;; Sample programs
+(def plus-3-minus-3-program ",+. This program will 6 characters\n,+. For first 3 characters it will\n,+. print its successor\n,-. For last 3 characters it will\n,-. print its predecessor\n,-.")
+(def hello-world-program "+++++ +++++             initialize counter (cell #0) to 10\n[                       use loop to set the next four cells to 70/100/30/10\n    > +++++ ++              add  7 to cell #1\n    > +++++ +++++           add 10 to cell #2\n    > +++                   add  3 to cell #3\n    > +                     add  1 to cell #4\n    <<<< -                  decrement counter (cell #0)\n]\n> ++ .                  print 'H'\n> + .                   print 'e'\n+++++ ++ .              print 'l'\n.                       print 'l'\n+++ .                   print 'o'\n> ++ .                  print ' '\n<< +++++ +++++ +++++ .  print 'W'\n> .                     print 'o'\n+++ .                   print 'r'\n----- - .               print 'l'\n----- --- .             print 'd'\n> + .                   print '!'")
 
+;; Constants
 (def default-tape-length 30000)
 (def operation-count-threshold 100000)
 (def language-symbols #{\> \< \, \. \- \+ \[ \]})
 
-(defn tokenize [code] (->> code
-                           (filter #(contains? language-symbols %))
-                           (apply str)
-                           (into [])))
+;; Brainfuck grammar
+;; EXPR = < | > | + | - | , | . | LOOP | EXPR | eps
+;; LOOP = [ EXPR ]
 
-(defn execute
-  [tokens tape ptr data]
-  (loop [i 0
-         tape tape
-         ptr ptr
-         data data
-         loop-i []
-         op-count 0]
-    (when (< i (count tokens))
-      (println (map #(int %) (take 5 tape)))
-      (if-not (< op-count operation-count-threshold)
-        (print "\nPROCESS TIME OUT. KILLED!!!")
-        (case (tokens i)
-          \> (recur (inc i) tape (inc ptr) data loop-i (inc op-count))
-          \< (recur (inc i) tape (dec ptr) data loop-i (inc op-count))
-          \, (recur (inc i)
-                    (update-in tape [ptr] (fn [_] (int (first data))))
-                    ptr (rest data) loop-i (inc op-count))
-          \. (do (print (char (tape ptr)))
-                 (recur (inc i) tape ptr data loop-i (inc op-count)))
-          \- (recur (inc i) (update-in tape [ptr] #(if (= % 0) 225 (dec %))) ptr data loop-i (inc op-count))
-          \+ (recur (inc i) (update-in tape [ptr] #(if (= % 255) 0 (inc %))) ptr data loop-i (inc op-count))
-          \[ (recur (inc i) tape ptr data (conj loop-i (inc i)) (inc op-count))
-          \] (if (= 0 (tape ptr))
-               ;; skip loop
-               (recur (inc i) tape ptr data (pop loop-i) (+ 2 op-count))
-               ;; jump to first loop instruction
-               (recur (peek loop-i) tape ptr data loop-i (+ 2 op-count))))))))
+(defn get-cell [runtime-metadata] ((:tape runtime-metadata) (:ptr runtime-metadata)))
+
+(defn update-cell
+  [runtime-metadata f]
+  (update-in runtime-metadata [:tape]
+             (fn [tape] (update-in tape [(:ptr runtime-metadata)] f))))
+
+(defmulti execute (fn [expr runtime-metadata]
+                    (if (< (:op-count runtime-metadata) operation-count-threshold)
+                      (::bf-expr expr)
+                      (throw (Exception. "\nPROCESS TIME OUT. KILLED!!!")))))
+
+(defmethod execute ::< [_ runtime-metadata] (-> runtime-metadata
+                                                (update-in [:ptr] dec)
+                                                (update-in [:op-count] inc)))
+(defmethod execute ::> [_ runtime-metadata] (-> runtime-metadata
+                                                (update-in [:ptr] inc)
+                                                (update-in [:op-count] inc)))
+(defmethod execute ::plus [_ runtime-metadata] (-> runtime-metadata
+                                                   (update-cell #(if (= % 255) 0 (inc %)))
+                                                   (update-in [:op-count] inc)))
+(defmethod execute ::minus [_ runtime-metadata] (-> runtime-metadata
+                                                    (update-cell #(if (= % 0) 255 (dec %)))
+                                                    (update-in [:op-count] inc)))
+(defmethod execute ::comma [_ runtime-metadata] (-> runtime-metadata
+                                                    (update-cell (fn [_] (int (first (:data runtime-metadata)))))
+                                                    (update-in [:data] rest)
+                                                    (update-in [:op-count] inc)))
+(defmethod execute ::dot [_ runtime-metadata] (do (print (char (get-cell runtime-metadata)))
+                                                  (update-in runtime-metadata [:op-count] inc)))
+(defmethod execute ::loop [loop runtime-metadata]
+  (let [cell-check (fn [runtime-metadata f]
+                     (let [runtime-metadata (update-in runtime-metadata [:op-count] inc)]
+                       (if-not (= 0 (get-cell runtime-metadata))
+                         (f runtime-metadata)
+                         runtime-metadata)))]
+    (-> runtime-metadata
+        ;; at [
+        (cell-check (fn [runtime-metadata] (execute (:exprs loop) runtime-metadata)))
+        ;; at ]
+        (cell-check (fn [runtime-metadata] (execute loop runtime-metadata))))))
+(defmethod execute :default [exprs runtime-metadata] (reduce (fn [runtime-metadata expr] (execute expr runtime-metadata))
+                                                             runtime-metadata
+                                                             exprs))
+
+(defn scan [code] (->> code
+                       (filter #(contains? language-symbols %))
+                       (apply str)
+                       (into [])))
+
+(defn parse
+  ([tokens] (parse tokens 0 []))
+  ([tokens start acc]
+   (if (< start (count tokens))
+     (case (tokens start)
+       \< (recur tokens (inc start) (conj acc {::bf-expr ::<}))
+       \> (recur tokens (inc start) (conj acc {::bf-expr ::>}))
+       \+ (recur tokens (inc start) (conj acc {::bf-expr ::plus}))
+       \- (recur tokens (inc start) (conj acc {::bf-expr ::minus}))
+       \, (recur tokens (inc start) (conj acc {::bf-expr ::comma}))
+       \. (recur tokens (inc start) (conj acc {::bf-expr ::dot}))
+       \[ (let [[end exprs] (parse tokens (inc start) [])]
+            (recur tokens (inc end) (conj acc {::bf-expr ::loop :exprs exprs})))
+       \] [start acc])
+     acc)))
 
 (defn read-code
-  [instruction-count]
+  [line-count]
   (loop [i 0 acc []]
-    (if (< i instruction-count)
+    (if (< i line-count)
       (recur (inc i) (conj acc (read-line)))
       (apply str acc))))
 
 (defn -main [& args]
-  (let [instruction-count (->> (clojure.string/split (read-line) #" ")
-                               ;; map to ints
-                               (map #(Integer/parseInt %))
-                               ;; skip first int, representing string length
-                               (second))
-        ;; remove last symbol ($) from the input string
-        data (drop-last (read-line))
-        code (read-code instruction-count)
+  (let [[input-count line-count] (->> (clojure.string/split (read-line) #" ")
+                                      ;; map to ints
+                                      (map #(Integer/parseInt %)))
+        ;; remove last symbol ($) from the input string. Read all input beforehand
+        data (take input-count (read-line))
+        ;; read the code
+        code (read-code line-count)
+        ;; allocate tape memory
         tape (into [] (repeat default-tape-length 0))
-        ptr 0]
-    (execute (tokenize code) tape ptr data)))
+        ;; runtime-metadata setup
+        runtime-metadata {:tape tape :ptr 0 :data data :op-count 0}]
+    (try
+      (execute (parse (scan code)) runtime-metadata)
+      (catch Exception e (print (.getMessage e))))))
